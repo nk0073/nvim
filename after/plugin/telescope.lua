cb0bbfcb0da32cb150e02880e5ae098390ba1758
73 <110295543+nk0073@users.noreply.github.com>
2025-06-22 17:11:02 -0400

    Update telescope.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,6 @@
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>pf', builtin.find_files, {}) -- search files
 vim.keymap.set('n', '<C-p>', builtin.git_files, {}) -- search git files
-vim.keymap.set('n', '<leader>ps', function() -- search word in files
+vim.keymap.set('n', '<leader>pw', function() -- search word in files
 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
